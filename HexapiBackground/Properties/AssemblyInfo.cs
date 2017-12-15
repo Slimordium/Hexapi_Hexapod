b01691b763ed3ada166d2e916f460e1e68eca2c7
Larry Watkins <lwatkins@colbi.com>
2017-12-14 19:11:01 -0700

    Updated for the latest net core uwp

--- HexapiBackground/Properties/AssemblyInfo.cs
+++ HexapiBackground/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("HexapiBackground")]
-[assembly: AssemblyDescription("Hexapod with support Inverse Kinematics, GPS, MPU, NTRIP, Ping Sensors, Xbox Controller, I2C")]
+[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("HexapiBackground")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
